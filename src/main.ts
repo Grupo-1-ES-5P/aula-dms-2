f652678809c321da0acf76d039370971228e4676
Alessandra <alessandravitoria799@gmail.com>
2026-03-30 20:46:23 -0300

    feat: implementação do swagger no modulo i guess

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT!);
+
+  app.setGlobalPrefix("v1");
+
+  const config = new DocumentBuilder()
+    .setTitle("Módulo de Chamada")
+    .setDescription("Módulo de Chamada.")
+    .setVersion("1.0")
+    .addBearerAuth({ type: "http", scheme: "bearer", bearerFormat: "JWT" })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 void bootstrap();
--- src/modules/attendance/infra/controllers/attendances.controller.ts
+++ src/modules/attendance/infra/controllers/attendances.controller.ts
@@ -3,11 +3,18 @@ import { AttendanceStatus } from "@attendance/domain/models/attendance.entity";
 import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { Permission } from "@shared/enums/permission.enum";
 import { RequirePermissions } from "@shared/infra/decorators/permissions.decorator";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiQuery, ApiNotFoundResponse, ApiNoContentResponse } from "@nestjs/swagger";
 
+@ApiTags("attendances")
+@ApiBearerAuth()
 @Controller("attendances")
 export class AttendancesController {
   constructor(private readonly attendanceService: AttendanceService) {}
 
+  @ApiOperation({ summary: "Listar presenças por turma ou por aluno e turma" })
+  @ApiQuery({ name: "class_offering_id", required: true, type: String })
+  @ApiQuery({ name: "student_id", required: false, type: String })
   @Get("student/:studentId/class-offering/:classOfferingId")
   //@RequirePermissions(Permission.ATTENDANCES_READ)
   async findByStudent(
@@ -20,6 +27,7 @@ export class AttendancesController {
     );
   }
 
+  
   @Get("class-offering/:classOfferingId")
   //@RequirePermissions(Permission.ATTENDANCES_READ)
   async findByClassOffering(@Param("classOfferingId") classOfferingId: string) {
@@ -27,6 +35,7 @@ export class AttendancesController {
   }
 
   @Post()
+  @ApiOperation({summary: "Registrar chamada/presença"})
   //@RequirePermissions(Permission.ATTENDANCES_WRITE)
   async register(
     @Body() body: {
