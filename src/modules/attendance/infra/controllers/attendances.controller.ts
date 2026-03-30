85e73f0b7942313f3a4128f709c13d74815e43bc
Izarut <eduardotpanata@gmail.com>
2026-03-30 20:44:33 -0300

    feat: aplica api standards no controller de attendances

--- src/modules/attendance/infra/controllers/attendances.controller.ts
+++ src/modules/attendance/infra/controllers/attendances.controller.ts
@@ -1,41 +1,31 @@
+import { CreateAttendanceDto } from "@attendance/application/dto/create-attendance.dto";
 import { AttendanceService } from "@attendance/application/services/attendance.service";
-import { AttendanceStatus } from "@attendance/domain/models/attendance.entity";
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
-import { Permission } from "@shared/enums/permission.enum";
-import { RequirePermissions } from "@shared/infra/decorators/permissions.decorator";
+import { Body, Controller, Get, Post, Query } from "@nestjs/common";
+// import { Permission } from "@shared/enums/permission.enum";
+// import { RequirePermissions } from "@shared/infra/decorators/permissions.decorator";
 
 @Controller("attendances")
 export class AttendancesController {
   constructor(private readonly attendanceService: AttendanceService) {}
 
-  @Get("student/:studentId/class-offering/:classOfferingId")
-  //@RequirePermissions(Permission.ATTENDANCES_READ)
-  async findByStudent(
-    @Param("studentId") studentId: string,
-    @Param("classOfferingId") classOfferingId: string,
+  @Get()
+  // @RequirePermissions(Permission.ATTENDANCES_READ)
+  async findAll(
+    @Query("class_offering_id") classOfferingId: string,
+    @Query("student_id") studentId?: string,
   ) {
-    return this.attendanceService.findByStudentAndClassOffering(
-      studentId,
-      classOfferingId,
-    );
-  }
-
-  @Get("class-offering/:classOfferingId")
-  //@RequirePermissions(Permission.ATTENDANCES_READ)
-  async findByClassOffering(@Param("classOfferingId") classOfferingId: string) {
+    if (studentId) {
+      return this.attendanceService.findByStudentAndClassOffering(
+        studentId,
+        classOfferingId,
+      );
+    }
     return this.attendanceService.findByClassOffering(classOfferingId);
   }
 
   @Post()
-  //@RequirePermissions(Permission.ATTENDANCES_WRITE)
-  async register(
-    @Body() body: {
-      studentId: string;
-      lessonId: string;
-      classOfferingId: string;
-      status: AttendanceStatus;
-    },
-  ) {
+  // @RequirePermissions(Permission.ATTENDANCES_WRITE)
+  async register(@Body() body: CreateAttendanceDto) {
     return this.attendanceService.register(body);
   }
 }
