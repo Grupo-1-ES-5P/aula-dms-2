b502c8df0946de1f148c8842b548f6dd30100e37
Alessandra <alessandravitoria799@gmail.com>
2026-05-02 10:22:24 -0300

    fix(app): iniciar servidor HTTP

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,10 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("docs", app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
 }
 
 void bootstrap();
