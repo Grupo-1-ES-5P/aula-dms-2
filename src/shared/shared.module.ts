830e9a857991c9d6ee902d7fc48e6ee9ceab91a3
Alessandra <alessandravitoria799@gmail.com>
2026-05-02 10:22:34 -0300

    feat(atendimento): publicar evento de presença

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { DrizzleService } from "./infra/database/drizzle.service";
+import { MessagingModule } from "./infra/messaging/messaging.module";
 
 @Module({
+  imports: [MessagingModule],
   providers: [DrizzleService],
-  exports: [DrizzleService],
+  exports: [DrizzleService, MessagingModule],
 })
 export class SharedModule {}
