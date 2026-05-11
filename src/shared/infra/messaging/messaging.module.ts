d1ab79343267dd8bac27c0bec4e61f64725d6d94
gustagueimer <gustavomarqueslima@outlook.com>
2026-05-11 17:24:04 -0300

    feat: criação das filas e exchanges de enrollment

--- src/shared/infra/messaging/messaging.module.ts
+++ src/shared/infra/messaging/messaging.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AttendancePublisherService } from "./attendance-publisher.service";
 import { ClassOfferingConsumerService } from "./class-offering-consumer.service";
+import { EnrollmentConsumerService } from "@shared/infra/messaging/enrollment-consumer.service";
 import { RabbitMQService } from "./rabbitmq.service";
 
 @Module({
@@ -10,11 +11,13 @@ import { RabbitMQService } from "./rabbitmq.service";
     RabbitMQService,
     AttendancePublisherService,
     ClassOfferingConsumerService,
+    EnrollmentConsumerService,
   ],
   exports: [
     RabbitMQService,
     AttendancePublisherService,
     ClassOfferingConsumerService,
+    EnrollmentConsumerService,
   ],
 })
 export class MessagingModule {}
